d601fb90c6275129c048769c68c6f8d126219370
Gary Hildebrand <ghilde@microsoft.com>
2016-12-19 22:03:03 -0800

    Make program for second day 1 puzzle work for problem input.

--- 01/01b.cpp
+++ 01/01b.cpp
@@ -1,16 +1,19 @@
 #include <stdio.h>
 
 #include "../common/InputReader.hpp"
+#include "../advent2016/GridMoveParser.hpp"
 
 using namespace Common;
+using namespace Advent2016;
 
 int main()
 {
 	char *line = NULL;
 	while ((line = InputReader::ReadLine()) != NULL)
 	{
-		// TODO: invoke class code here
-		(void)printf("%s\n", line);
-	}
+        GridMoveParser parser;
+        parser.parse(line);
+        (void)printf("%u\n", parser.getDistanceToFirstRevisit());
+    }
 	return 0;
 }
