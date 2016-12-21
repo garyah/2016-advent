99fbe8ca7e0c453607a615071d813b4d76892fd0
Gary Hildebrand <ghilde@microsoft.com>
2016-12-20 19:20:24 -0800

    Finish class enough for unit test for first day 4 puzzle to pass.

--- advent2016/RoomValidator.hpp
+++ advent2016/RoomValidator.hpp
@@ -1,9 +1,14 @@
 #include <stdio.h>
+#include <ctype.h>
+#include <map>
 
 namespace Advent2016
 {
 	class RoomValidator
 	{
+        typedef std::map<char, unsigned> CountMap;
+        typedef std::map<unsigned, std::string> LettersMap;
+
     public:
         RoomValidator() :
             m_sumOfIds(0)
@@ -12,8 +17,18 @@ namespace Advent2016
 
         void addRoom(char *input)
         {
-            //(void)sscanf(input, "%d %d %d", &side1, &side2, &side3);
-            //m_sumOfIds += 888;
+            char roomName[1000] = { 0 };
+            int id = -1;
+            char roomChecksum[1000] = { 0 };
+            (void)sscanf(input, "%999[-abcdefghijklmnopqrstuvwxyz]%u[%999[abcdefghijklmnopqrstuvwxyz]]", roomName, &id, roomChecksum);
+            if (id >= 0)
+            {
+                auto result = calculateChecksum(roomName);
+                if (0 == result.compare(roomChecksum))
+                {
+                    m_sumOfIds += static_cast<unsigned>(id);
+                }
+            }
         }
 
         unsigned getSumOfIds()
@@ -22,6 +37,25 @@ namespace Advent2016
         }
 
     private:
+        std::string calculateChecksum(const char *roomName)
+        {
+            CountMap countMap;
+            for (auto p = roomName; *p; ++p)
+            {
+                if (islower(*p)) { countMap[*p]++; }
+            }
+            LettersMap lettersMap;
+            for (auto i = countMap.begin(); i != countMap.end(); ++i)
+            {
+                lettersMap[i->second].append(1, i->first);
+            }
+            std::string result;
+            for (auto i = lettersMap.rbegin(); i != lettersMap.rend(); ++i)
+            {
+                result.append(i->second);
+            }
+            return result.substr(0, 5);
+        }
 
     private:
         unsigned m_sumOfIds;
