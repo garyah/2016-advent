03accbc50809f650bef25d869b5fa13748a6c19e
Gary Hildebrand <ghilde@microsoft.com>
2016-12-21 10:24:02 -0800

    Add projects for day 21 puzzles.  New class PasswordScrambler with unit test.  Unit test from puzzle initially fails, since implementation is empty.  Added classes to executable for first day 21 puzzle, to make it work for problem input, also added.

---
+++ advent2016-tests/PasswordScramblerTests.cpp
@@ -0,0 +1,25 @@
+#include "stdafx.h"
+#include "CppUnitTest.h"
+
+#include "../advent2016/PasswordScrambler.hpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+using namespace Advent2016;
+
+namespace advent2016tests
+{		
+	TEST_CLASS(PasswordScramblerTests)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod21a1)
+		{
+            PasswordScrambler scrambler;
+            //scrambler.addRule("");
+            //scrambler.addRule("");
+            scrambler.scramble("abcde");
+            Assert::AreEqual("decab", scrambler.getPassword());
+		}
+
+    };
+}
