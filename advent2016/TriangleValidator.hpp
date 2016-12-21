4760171bfcb807a5b06bb846b9d71cb3ef86453f
Gary Hildebrand <ghilde@microsoft.com>
2016-12-20 16:59:50 -0800

    Finish class enough for unit test for first day 3 puzzle to pass.

--- advent2016/TriangleValidator.hpp
+++ advent2016/TriangleValidator.hpp
@@ -13,6 +13,17 @@ namespace Advent2016
 
         void addTriangle(char *input)
         {
+            int side1 = -1, side2 = -1, side3 = -1;
+            (void)sscanf(input, "%d %d %d", &side1, &side2, &side3);
+            if (side1 >= 0 && side2 >= 0 && side3 >= 0)
+            {
+                if (side1 + side2 > side3
+                    && side1 + side3 > side2
+                    && side2 + side3 > side1)
+                {
+                    ++m_numValid;
+                }
+            }
         }
 
         unsigned getNumValid()
