d532bde10d6d01ffab39418013083d3938709c01
Gary Hildebrand <ghilde@microsoft.com>
2016-12-29 08:23:43 -0800

    Debugging, which reveals why Debug version of second day 16 puzzle takes so long.  Basic std::string operations such as substr(), compare(), and append() seem to take much longer in Debug than Release.

--- advent2016/ChecksumGenerator.hpp
+++ advent2016/ChecksumGenerator.hpp
@@ -1,3 +1,5 @@
+#define DEBUG_TEST
+
 #include <string>
 
 namespace Advent2016
@@ -9,6 +11,9 @@ namespace Advent2016
         static std::string expand(const char *data, size_t targetLength)
         {
             std::string result(data);
+#ifdef DEBUG_TEST
+            unsigned count = 0;
+#endif // DEBUG_TEST
             while (result.length() < targetLength)
             {
                 std::string copy(result);
@@ -22,6 +27,10 @@ namespace Advent2016
                 if (copy.length() & 1) { copy[i] = invert(copy[i]); }
                 result.append("0");
                 result.append(copy);
+#ifdef DEBUG_TEST
+                ++count;
+                if (!(count % 1)) { (void)printf("expand() iteration #%u, length %zu\n", count, result.length()); }
+#endif // DEBUG_TEST
             }
             result = result.substr(0, targetLength);
             return result;
@@ -31,9 +40,15 @@ namespace Advent2016
         {
             std::string result(data);
             if (result.length() & 1) return result;
+#ifdef DEBUG_TEST
+            unsigned count1 = 0;
+#endif // DEBUG_TEST
             while (!(result.length() & 1))
             {
                 std::string temp;
+#ifdef DEBUG_TEST
+                unsigned count2 = 0;
+#endif // DEBUG_TEST
                 for (size_t i = 0; i < result.length(); i += 2)
                 {
                     if (result.substr(i, 2).compare("00") == 0
@@ -46,8 +61,16 @@ namespace Advent2016
                     {
                         temp.append("0");
                     }
+#ifdef DEBUG_TEST
+                    ++count2;
+                    if (!(count2 % 100000)) { (void)printf("generate() inner iteration #%u, length %zu\n", count2, temp.length()); }
+#endif // DEBUG_TEST
                 }
                 result = temp;
+#ifdef DEBUG_TEST
+                ++count1;
+                if (!(count1 % 1)) { (void)printf("generate() iteration #%u, length %zu\n", count1, result.length()); }
+#endif // DEBUG_TEST
             }
             return result;
         }
