c9f162d7ea54918cf64ad8757bf3d9a14f9db97f
Gary Hildebrand <ghilde@microsoft.com>
2016-12-28 23:53:48 -0800

    New static methods to invert and expand input data, the latter to a particular target length.  Unit tests added for expansion based on puzzle pass.

--- advent2016-tests/ChecksumGeneratorTests.cpp
+++ advent2016-tests/ChecksumGeneratorTests.cpp
@@ -7,16 +7,33 @@ using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 using namespace Advent2016;
 
 namespace advent2016tests
-{		
+{
     TEST_CLASS(ChecksumGeneratorTests)
     {
     public:
         
-        TEST_METHOD(TestMethod16a1)
+        TEST_METHOD(ExpandOneOneCorrect)
         {
-            ChecksumGenerator generator;
-            generator.generate("10000", 20);
-            Assert::AreEqual("01100", generator.getChecksum());
+            auto result = ChecksumGenerator::expand("1", 3);
+            Assert::AreEqual("100", result.c_str());
+        }
+
+        TEST_METHOD(ExpandOneZeroCorrect)
+        {
+            auto result = ChecksumGenerator::expand("0", 3);
+            Assert::AreEqual("001", result.c_str());
+        }
+
+        TEST_METHOD(ExpandFiveOnesCorrect)
+        {
+            auto result = ChecksumGenerator::expand("11111", 11);
+            Assert::AreEqual("11111000000", result.c_str());
+        }
+
+        TEST_METHOD(ExpandMixedOnesZeroesCorrect)
+        {
+            auto result = ChecksumGenerator::expand("111100001010", 25);
+            Assert::AreEqual("1111000010100101011110000", result.c_str());
         }
 
     };
--- advent2016/ChecksumGenerator.hpp
+++ advent2016/ChecksumGenerator.hpp
@@ -5,12 +5,40 @@ namespace Advent2016
     class ChecksumGenerator
     {
     public:
+
+        static std::string expand(const char *data, size_t targetLength)
+        {
+            std::string result(data);
+            while (result.length() < targetLength)
+            {
+                std::string copy(result);
+                size_t i = 0;
+                for (size_t j = copy.length() - 1; i < copy.length() / 2; ++i, --j)
+                {
+                    auto temp = (invert(copy[i]));
+                    copy[i] = invert(copy[j]);
+                    copy[j] = temp;
+                }
+                if (copy.length() & 1) { copy[i] = invert(copy[i]); }
+                result.append("0");
+                result.append(copy);
+            }
+            result = result.substr(0, targetLength);
+            return result;
+        }
+
+        static std::string generate(const char *data)
+        {
+            std::string result;
+            return result;
+        }
+
         ChecksumGenerator() :
             m_checksum()
         {
         }
 
-        void generate(const char *startData, size_t targetLength = 272)
+        void expandAndGenerate(const char *startData, size_t targetLength = 272)
         {
         }
 
@@ -20,6 +48,12 @@ namespace Advent2016
         }
 
     private:
+        static char invert(char data)
+        {
+            if (data == '0') return '1';
+            if (data == '1') return '0';
+            return data;
+        }
 
     private:
         std::string m_checksum;
